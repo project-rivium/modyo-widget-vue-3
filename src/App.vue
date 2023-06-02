4dc00de69322c086bf217161281fe7b789e63aca
luisfalconmx <luisfalconmx@gmail.com>
2023-06-01 23:13:12 -0600

    feat: project setup with vite, tailwindcss and linters

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main
+    class="flex h-screen flex-col justify-center bg-neutral-900 bg-[url('https://github.com/luisfalconmx/luisfalconmx.dev/assets/57654255/22a18938-ad60-4798-83e7-6c85f753562e')] bg-cover text-white"
+  >
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+      alt="create-spa-app"
+      class="mx-auto max-w-[140px]"
+    />
+    <p class="mx-auto mt-3 max-w-lg text-center text-2xl font-bold text-white">
+      Your new Modyo Widget with a configuration for VueJS 3
+    </p>
+    <img
+      src="https://github.com/luisfalconmx/luisfalconmx.dev/assets/57654255/62fcd4f4-c06a-428d-ac0b-914da90ed339"
+      alt="stack"
+      class="mx-auto mt-6"
+    />
+    <p class="mx-auto mt-12 block w-fit text-white">
+      Created with ❤️ by
+      <a class="font-bold underline" href="https://github.com/luisfalconmx">
+        luisfalconmx
+      </a>
+    </p>
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
