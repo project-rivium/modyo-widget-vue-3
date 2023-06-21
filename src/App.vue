7a52be19705cf3bdb0b7f989c558d48d52b4174e
luisfalconmx <luisfalconmx@gmail.com>
2023-06-21 10:38:34 -0600

    build: rename package and update npm commands

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
       alt="create-spa-app"
-      class="mx-auto max-w-[140px]"
+      class="mx-auto mb-7 block max-w-[140px]"
     />
-    <p class="mx-auto mt-3 max-w-lg text-center text-2xl font-bold text-white">
+    <p class="mx-auto max-w-lg text-center text-2xl font-bold text-white">
       Your new Modyo Widget with a configuration for VueJS 3
     </p>
     <img
@@ -15,26 +15,30 @@
       alt="stack"
       class="mx-auto mt-6"
     />
-    <p class="mx-auto mt-12 block w-fit text-white">
-      Created with
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="inline-block h-5 w-5"
+    <a
+      href="https://rivium-docs.surge.sh/modyo-widgets-template-vue/"
+      target="_blank"
+      class="mx-auto mt-12 block w-fit text-white"
+    >
+      <button
+        class="flex items-center rounded bg-emerald-500 px-4 py-2 font-bold hover:bg-emerald-600"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-        />
-      </svg>
-      by
-      <a class="font-bold underline" href="https://github.com/luisfalconmx">
-        luisfalconmx
-      </a>
-    </p>
+        <span class="mr-2 block">Getting Started</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="3"
+          stroke="currentColor"
+          class="h-4 w-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+          />
+        </svg>
+      </button>
+    </a>
   </main>
 </template>
